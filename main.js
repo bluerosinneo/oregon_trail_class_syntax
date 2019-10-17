f861bf09fe368f2ee20346eeab5719b8b370d5c4
Grimes <crameg@amazon.com>
2019-10-17 15:04:14 -0400

    change to .prototype.name = function...

--- main.js
+++ main.js
@@ -5,47 +5,52 @@
         this.name = name;
         this.food = 1;
         this.isHealthy = true;
-        this.hunt = function(){
-            this.food = this.food + 2;
-        };
-        this.eat = function(){
-            if(this.food >= 1){
-                this.food = this.food - 1;
-            }
-            else{
-                this.isHealthy = false;
-            }
-        }
     }
+
+    Traveler.prototype.hunt = function(){
+        this.food = this.food + 2;
+    };
+    Traveler.prototype.eat = function(){
+        if(this.food >= 1){
+            this.food = this.food - 1;
+        }
+        else{
+            this.isHealthy = false;
+        }
+    };
+
+    
 
     function Wagon(capacity){
         this.capacity = capacity;
         this.passengers = [];
-        this.getAvailableSeatCount = function(){
-            return this.capacity - this.passengers.length;
-        };
-        this.join = function(traveler){
-            if(this.getAvailableSeatCount() > 0){
-                this.passengers.push(traveler);
-            }
-        };
-        this.shouldQuarantine = function(){
-            let quarantine = false;
-            for(let i = 0; i < this.passengers.length; i++){
-                if(this.passengers[i].isHealthy == false){
-                    quarantine = true;
-                }
-            }
-            return quarantine;
-        };
-        this.totalFood = function(){
-            let food = 0;
-            for(let i = 0; i < this.passengers.length; i++){
-                food = food + this.passengers[i].food;
-            }
-            return food;
-        }
+
     }
+
+    Wagon.prototype.getAvailableSeatCount = function(){
+        return this.capacity - this.passengers.length;
+    };
+    Wagon.prototype.join = function(traveler){
+        if(this.getAvailableSeatCount() > 0){
+            this.passengers.push(traveler);
+        }
+    };
+    Wagon.prototype.shouldQuarantine = function(){
+        let quarantine = false;
+        for(let i = 0; i < this.passengers.length; i++){
+            if(this.passengers[i].isHealthy == false){
+                quarantine = true;
+            }
+        }
+        return quarantine;
+    };
+    Wagon.prototype.totalFood = function(){
+        let food = 0;
+        for(let i = 0; i < this.passengers.length; i++){
+            food = food + this.passengers[i].food;
+        }
+        return food;
+    };
 
     // Create a wagon that can hold 2 people
     let wagon = new Wagon(2);
